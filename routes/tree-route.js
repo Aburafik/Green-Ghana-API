4491c9019e3452c9f8fa5abce46e32ff4211402c
Rafik <rfkabubakari@gmail.com>
2023-07-30 23:28:36 +0000

    Filter trees

--- routes/tree-route.js
+++ routes/tree-route.js
@@ -14,45 +14,57 @@ const verifyToken = (req, res, next) => {
     if (err) {
       return res.status(401).json({ error: "Unauthorized" });
     }
-    req.userId=decodedToken.userId;
+    req.userId = decodedToken.userId;
     next();
   });
 };
 ///Register a new Tree
-treeRouter.post("/register/tree",verifyToken, async (req, res) => {
-         try {
-          const {treeName,treeHeight,locationPlanted,datePlanted}=req.body;
-          const owner=req.userId;
-          const tree= await Tree({treeName, treeHeight,locationPlanted, datePlanted ,owner});
-          await tree.save();
-          res.status(201).json({message:"Tree registered successfully", tree});
-          
-         } catch (error) {
-          res.status(500).json({error: error.message});
-         }
-
-
+treeRouter.post("/register/tree", verifyToken, async (req, res) => {
+  try {
+    const { treeName, treeHeight, locationPlanted, datePlanted } = req.body;
+    const owner = req.userId;
+    const tree = await Tree({
+      treeName:treeName[0].toUpperCase()+treeName.slice(1),
+      treeHeight,
+      locationPlanted,
+      datePlanted,
+      owner,
+    });
+    await tree.save();
+    res.status(201).json({ message: "Tree registered successfully", tree });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // View all trees
-treeRouter.get('/trees', verifyToken, async (req, res) => {
-          try {
-            const trees = await Tree.find({}).populate('owner', 'name');
-            res.status(200).json(trees);
-          } catch (error) {
-            res.status(500).json({ error: 'Error fetching trees' });
-          }
-        });
+treeRouter.get("/trees", verifyToken, async (req, res) => {
+  try {
+    const trees = await Tree.find({}).populate("owner", "name");
+    res.status(200).json(trees);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching trees" });
+  }
+});
 
-        // Filter trees by owner
-treeRouter.get('/trees/:ownerId', verifyToken, async (req, res) => {
-          try {
-            const { ownerId } = req.params;
-            const trees = await Tree.find({ owner: ownerId }).populate('owner', 'name');
-            res.status(200).json(trees);
-          } catch (error) {
-            res.status(500).json({ error: 'Error filtering trees' });
-          }
-        });
-        
+// Filter trees by
+treeRouter.get("/trees/:ownerId", verifyToken, async (req, res) => {
+  try {
+    const { ownerId } = req.params;
+    const trees = await Tree.find({ owner: ownerId }).populate("owner", "name");
+    res.status(200).json(trees);
+  } catch (error) {
+    res.status(500).json({ error: "Error filtering trees"});
+  }
+});
+
+treeRouter.get("/filter/:search", verifyToken, async (req, res) => {
+  try {
+    const filter = await Tree.find({ treeName: { $regex: req.params.search[0].toUpperCase()+ req.params.search.slice(1)} });
+    res.status(200).json(filter);
+  } catch (error) {
+    res.status(500).json({ error: "Error filtering tree by name" });
+  }
+});
+
 module.exports = treeRouter;
